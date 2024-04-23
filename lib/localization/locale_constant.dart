75b3a521a6163fe0e46a8f7dc56810428ed66b8e
mallikaroy <mallika.roy@bajajfinserv.in>
2024-04-23 15:33:56 +0530

    fixed issue of sharedPreference for iOS

--- lib/localization/locale_constant.dart
+++ lib/localization/locale_constant.dart
@@ -2,20 +2,19 @@ import 'dart:ui';
 
 import 'package:feburary_flutter/main.dart';
 import 'package:flutter/cupertino.dart';
-// import 'package:shared_preferences/shared_preferences.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 const String prefSelectedLanguageCode = "SelectedLanguageCode";
 
 Future<Locale> setLocale(String languageCode) async {
-  // // SharedPreferences _prefs = await SharedPreferences.getInstance();
-  // await _prefs.setString(prefSelectedLanguageCode, languageCode);
+  SharedPreferences _prefs = await SharedPreferences.getInstance();
+  await _prefs.setString(prefSelectedLanguageCode, languageCode);
   return _locale(languageCode);
 }
 
 Future<Locale> getLocale() async {
-  // SharedPreferences _prefs = await SharedPreferences.getInstance();
-  // String languageCode = _prefs.getString(prefSelectedLanguageCode) ?? "en";
-  String languageCode =  "en";
+  SharedPreferences _prefs = await SharedPreferences.getInstance();
+  String languageCode = _prefs.getString(prefSelectedLanguageCode) ?? "en";
 
   return _locale(languageCode);
 }

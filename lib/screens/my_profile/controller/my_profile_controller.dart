2e051a653e6a15db6cb1c4aff6d53e649379208f
mallikaroy <mallika.roy@bajajfinserv.in>
2024-05-14 11:43:14 +0530

    add architectural pattern of View, Binding, Controller

---
+++ lib/screens/my_profile/controller/my_profile_controller.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/cupertino.dart';
+import 'package:get/get.dart';
+
+class MyProfileController extends SuperController{
+
+  final mobileController = TextEditingController();
+  final mobileError = "".obs;
+
+  @override
+  void onDetached() {
+  }
+
+  @override
+  void onHidden() {
+  }
+
+  @override
+  void onInactive() {
+  }
+
+  @override
+  void onPaused() {
+  }
+
+  @override
+  void onResumed() {
+  }
+
+}
